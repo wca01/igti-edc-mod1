af62c84eb467a7dcc9622a0a396f7b76d8060a48
wca01 <welbboy@live.com>
2022-05-14 15:39:37 -0300

    fix error bucket dl

--- infrastructure/s3_files.tf
+++ infrastructure/s3_files.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket_object" "job_spark" {
-    bucket = aws_s3_bucket.datalake.id
+    bucket = aws_s3_bucket.dl.id
     key = "emr-code/pyspark/job_spark_from_tf.py"
     acl = "private"
     source = "./job_spark.py"
